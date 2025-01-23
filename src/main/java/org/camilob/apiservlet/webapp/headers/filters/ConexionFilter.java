290fb3f026d68c233668401013f5f46792412638
Camilo <cristianc-56@hotmai.com>
2025-01-23 08:57:14 -0500

    Pool de conexiones con Tomcat Datasource

--- src/main/java/org/camilob/apiservlet/webapp/headers/filters/ConexionFilter.java
+++ src/main/java/org/camilob/apiservlet/webapp/headers/filters/ConexionFilter.java
@@ -4,8 +4,9 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletResponse;
 import org.camilob.apiservlet.webapp.headers.services.ServiceJdbcException;
-import org.camilob.apiservlet.webapp.headers.utils.ConexionBaseDatos;
+import org.camilob.apiservlet.webapp.headers.utils.ConexionBaseDatosDS;
 
+import javax.naming.NamingException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -15,7 +16,7 @@ public class ConexionFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        try (Connection conn = ConexionBaseDatos.getConnection()){
+        try (Connection conn = ConexionBaseDatosDS.getConnection()){
 
             if (conn.getAutoCommit()){
                 conn.setAutoCommit(false);
@@ -30,7 +31,7 @@ public class ConexionFilter implements Filter {
                 e.printStackTrace();
             }
 
-        } catch (SQLException throwables){
+        } catch (SQLException | NamingException throwables){
             throwables.printStackTrace();
         }
     }
